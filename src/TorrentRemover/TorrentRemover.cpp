1a42045d8fe53e42a0cb0a912e283539badf9606
Rich Gibson <richgibson86@gmail.com>
2025-10-09 20:48:42 +0100

    Add recursion

--- src/TorrentRemover/TorrentRemover.cpp
+++ src/TorrentRemover/TorrentRemover.cpp
@@ -8,7 +8,7 @@ void TorrentRemover::RemoveTorrents()
     std::filesystem::path home{std::getenv("HOME")};
     home /= "Downloads";
 
-    std::filesystem::directory_iterator download_iterator{home};
+    std::filesystem::recursive_directory_iterator download_iterator{home};
 
     for (auto &&item : download_iterator)
     {
